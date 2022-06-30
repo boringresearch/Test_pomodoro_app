7c4541b632fca474649ee43a25f7cdd9b1ca850e
xihajun <jh2259@mrc-cu.cam.ac.uk>
2022-06-30 19:11:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,10 +51,10 @@ import psycopg2
 conn = psycopg2.connect(os.environ["DATABASE_URL"])
 
 with conn.cursor() as cur: 
-	cur.execute("SELECT now()") 
-	res = cur.fetchall() 
-	conn.commit() 
-	print(res)
+    cur.execute("SELECT now()") 
+    res = cur.fetchall() 
+    conn.commit() 
+    print(res)
     print(mins)
 
 if button_clicked:
